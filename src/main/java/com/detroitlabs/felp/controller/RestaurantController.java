4250c5a7815bfc92536359a191890a504e975c21
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-02-27 15:36:30 -0500

    adds method to get restaurant by name, created details page, able to successfully show details page

--- src/main/java/com/detroitlabs/felp/controller/RestaurantController.java
+++ src/main/java/com/detroitlabs/felp/controller/RestaurantController.java
@@ -2,9 +2,11 @@ package com.detroitlabs.felp.controller;
 
 import com.detroitlabs.felp.data.RestaurantRepository;
 import com.detroitlabs.felp.model.Restaurant;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +25,12 @@ public class RestaurantController {
 
         modelMap.put("allRestaurants", allRestaurants);
         return "home";
+    }
+
+    @RequestMapping("/restaurant/{name}")
+    public String restaurantDetails(@PathVariable String name, ModelMap modelMap){
+        Restaurant restaurant = restaurantRepository.findByName(name);
+        modelMap.put("restaurant", restaurant);
+        return "restaurant-details";
     }
 }
--- src/main/java/com/detroitlabs/felp/data/RestaurantRepository.java
+++ src/main/java/com/detroitlabs/felp/data/RestaurantRepository.java
@@ -10,13 +10,22 @@ import java.util.List;
 public class RestaurantRepository {
 
     private static final List<Restaurant> ALL_RESTAURANTS = Arrays.asList(
-      new Restaurant("123 Easy Street", "Detroit", "Michigan", "Chicken Hut", "Chicken", "2483549834", 100, "Local Ma' and Pa' Shop"),
-      new Restaurant("145 Easy Street", "Rochester", "Michigan", "Shrimp Hut", "Shrimp", "2466549834", 50, "Local Ma' and Pa' Shop"),
-      new Restaurant("532 Easy Street", "Troy", "Michigan", "Fish Hut", "Fish", "2483549234", 60, "Local Ma' and Pa' Shop")
+      new Restaurant("123 Easy Street", "Detroit", "Michigan", "ChickenHut", "Chicken", "2483549834", 100, "Local Ma' and Pa' Shop"),
+      new Restaurant("145 Easy Street", "Rochester", "Michigan", "ShrimpHut", "Shrimp", "2466549834", 50, "Local Ma' and Pa' Shop"),
+      new Restaurant("532 Easy Street", "Troy", "Michigan", "FishHut", "Fish", "2483549234", 60, "Local Ma' and Pa' Shop")
     );
 
     public List<Restaurant> getAllRestaurants() {
         return ALL_RESTAURANTS;
+    }
+
+    public Restaurant findByName(String name){
+        for (Restaurant restaurant: ALL_RESTAURANTS) {
+            if(restaurant.getName().equalsIgnoreCase(name)) {
+                return restaurant;
+            }
+        }
+        return null;
     }
 
 }
